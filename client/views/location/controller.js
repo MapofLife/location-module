fc9de872124b1483e481cd0e47937ba9375da2ae
Jeremy Malczyk <jmalczyk@gmail.com>
2015-10-01 16:29:11 -0400

    You will make this mistake, I guarantee it.

--- client/views/location/controller.js
+++ client/views/location/controller.js
@@ -3,12 +3,14 @@ angular.module('mol.region-model-ctrl',[])
     '$scope','MOLSpeciesList',
 
     function($scope,MOLSpeciesList) {
-      $scope.region = undefined; //the region of interest
-      $scope.taxa = undefined; //the list of taxa
-      $scope.taxon = undefined; //the selected taxon (species list)
-      $scope.species = undefined; //the selected species
-      $scope.filters = {"elev" : {min:-500,max:8500}}
-      $scope.$watch('region',function(newValue,oldValue){
+      $scope.model =
+        {region : undefined, //the region of interest
+        taxa : undefined, //the list of taxa
+        taxon : undefined, //the selected taxon (species list)
+        species : undefined, //the selected species
+        filters : {"elev" : {min:-500,max:8500}}};
+
+      $scope.$watch('model.region',function(newValue,oldValue){
          if(newValue) {
             MOLSpeciesList.searchRegion(newValue).then(
                function(result) {
