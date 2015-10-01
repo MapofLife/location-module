8d0117ae1c80dca022182e46d69b8f622c5e6d75
Jeremy Malczyk <jmalczyk@gmail.com>
2015-10-01 16:00:13 -0400

    Adding some more models.

--- client/views/location/controller.js
+++ client/views/location/controller.js
@@ -3,8 +3,11 @@ angular.module('mol.region-model-ctrl',[])
     '$scope','MOLSpeciesList',
 
     function($scope,MOLSpeciesList) {
-      $scope.region = undefined;
-      $scope.taxa = undefined;
+      $scope.region = undefined; //the region of interest
+      $scope.taxa = undefined; //the list of taxa
+      $scope.taxon = undefined; //the selected taxon (species list)
+      $scope.species = undefined; //the selected species
+      $scope.filters = {"elev" : {min:-500,max:8500}}
       $scope.$watch('region',function(newValue,oldValue){
          if(newValue) {
             MOLSpeciesList.searchRegion(newValue).then(
